52986cadc8b5611e92f2460a80be111479670b8a
Nguyen Nhat <nldnhat182006@gmail.com>
2026-03-31 19:53:09 +0700

    fix: add RequestMapping

--- src/main/java/com/SmartBank/controller/TransactionController.java
+++ src/main/java/com/SmartBank/controller/TransactionController.java
@@ -9,9 +9,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/transactions")
 public class TransactionController {
 
     private final TransactionService service;
@@ -20,19 +22,19 @@ public class TransactionController {
         this.service = service;
     }
 
-    @PostMapping("/api/transactions/deposit")
+    @PostMapping("/deposit")
     public ResponseEntity<TransactionResponse> deposit(@Valid @RequestBody DepositWithDrawRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(service.deposit(request));
     }
 
-    @PostMapping("/api/transactions/withdraw")
+    @PostMapping("/withdraw")
     public ResponseEntity<TransactionResponse> withdraw(@Valid @RequestBody DepositWithDrawRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(service.withdraw(request));
     }
 
-    @PostMapping("/api/transactions/transfer")
+    @PostMapping("/transfer")
     public ResponseEntity<TransactionResponse> transfer(@Valid @RequestBody TransferRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(service.transfer(request));

90d33d62c2f2de35e3d877caeb18379402081b57
Nguyen Nhat <nldnhat182006@gmail.com>
2026-04-01 12:16:56 +0700

    feat: add AuthService

---
+++ src/main/java/com/SmartBank/service/AuthService.java
@@ -0,0 +1,30 @@
+package com.SmartBank.service;
+
+
+import com.SmartBank.dto.request.LoginRequest;
+import com.SmartBank.dto.response.LoginResponse;
+import com.SmartBank.model.Employee;
+import com.SmartBank.repository.EmployeeRepository;
+import com.SmartBank.util.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final EmployeeRepository employeeRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
+    public LoginResponse login(LoginRequest request) {
+        Employee employee = employeeRepository.findByUsername(request.getUsername()).orElseThrow(() -> new UsernameNotFoundException("User not found: " + request.getUsername()));
+
+        if(!passwordEncoder.matches(request.getPassword(), employee.getPassword())) {
+            throw new UsernameNotFoundException("Password incorrect");
+        }
+        String token = jwtUtil.generateToken(employee.getUsername(), employee.getRole().name());
+        return new LoginResponse(token, employee.getUsername(), employee.getRole().name());
+    }
+}

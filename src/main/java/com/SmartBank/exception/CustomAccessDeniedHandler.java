8cfbb2c16914f38884a9b5bcf9eaa5051c16fb14
Nguyen Nhat <nldnhat182006@gmail.com>
2026-04-01 13:16:34 +0700

    feat: add handle 401 and 403 exception

---
+++ src/main/java/com/SmartBank/exception/CustomAccessDeniedHandler.java
@@ -0,0 +1,30 @@
+package com.SmartBank.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest request,
+                       HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException {
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.getWriter().write("""
+            {
+                "timestamp": "%s",
+                "status": 403,
+                "message": "Access denied",
+                "path": "%s"
+            }
+            """.formatted(LocalDateTime.now(), request.getRequestURI()));
+    }
+}
---
+++ src/main/java/com/SmartBank/exception/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,29 @@
+package com.SmartBank.exception;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException)
+            throws IOException, ServletException {
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.getWriter().write("""
+                {
+                    "timestamp": "%s",
+                    "status": 401,
+                    "message": "Authentication required",
+                    "path": "%s"
+                }
+                """.formatted(LocalDateTime.now(), request.getRequestURI()));
+    }
+}

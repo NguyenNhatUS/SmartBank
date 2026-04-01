d5cdd1a5ddc59917f74d7aa2c2999f6091e31579
Nguyen Nhat <nldnhat182006@gmail.com>
2026-04-01 11:59:19 +0700

    feat: add JwtUtil for generate and verify token

--- src/main/java/com/SmartBank/util/JwtUtil.java
+++ src/main/java/com/SmartBank/util/JwtUtil.java
@@ -1,16 +1,12 @@
 package com.SmartBank.util;
 
-
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
 import javax.crypto.SecretKey;
-import java.security.Key;
 import java.util.Date;
 
 @Component
@@ -20,7 +16,6 @@ public class JwtUtil {
 
     @Value("${jwt.expiration}")
     private long expiration;
-
 
     public SecretKey getSigningKey() {
         return Keys.hmacShaKeyFor(secretKey.getBytes());
@@ -65,7 +60,5 @@ public class JwtUtil {
             return false;
         }
     }
-
-
 
 }
